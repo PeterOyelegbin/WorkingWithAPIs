4f32ff7167728800adf681c7a1e9000b73c8bda3
Peter Oyelegbin <peteroyelegbin@gmail.com>
2022-07-02 15:37:49 +0100

    update model.py

--- I4G006138YOT/links/models.py
+++ I4G006138YOT/links/models.py
@@ -1,16 +1,17 @@
-import django
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from .utils import generate_random_id
 
 # Create your models here.
 class Link(models.Model):
     target_url = models.URLField(max_length=200)
     descriptions = models.CharField(max_length=200)
     identifier = models.SlugField(max_length=20, blank=True, unique=True)
-    # author : A Foreign Key to the current user model. Make use of Django’s get_user_model function.
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.target_url
+    # Custom save method
+    def save(self, *args, **kwargs):
+        self.identifier = generate_random_id()
+        super(Link, self).save(*args, **kwargs)
